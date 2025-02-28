20db6d2d64b0659584465f1e7b1014327caf6837
Romik <romik.sargsyan@picsart.academy>
2025-02-28 17:24:13 +0400

    fixed some issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Ensure that fonts are handled correctly in production
+      config.module.rules.push({
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000, // Adjust the size limit if needed
+            name: "/static/fonts/[name].[hash].[ext]",
+          },
+        },
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
